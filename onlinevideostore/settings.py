8ee10ee5c08d56e5f1e974649b4e762aca7b2c0a
MezoneOrange <mezor07@mail.ru>
2020-06-20 12:24:57 +0500

    Bug fixes

--- onlinevideostore/settings.py
+++ onlinevideostore/settings.py
@@ -131,7 +131,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-LOGIN_URL = 'user'
+LOGIN_URL = 'auth'
 LOGIN_REDIRECT_URL = 'home'
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
